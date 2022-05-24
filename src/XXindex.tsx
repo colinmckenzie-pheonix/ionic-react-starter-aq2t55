1d00b888e5e37d339b2a33cbee114089981bbf32
colinmckenzie-pheonix <colin_m_mckenzie@standardlife.com>
2022-05-24 21:19:33 +0100

    5 files modified, 1 added, 1 deleted

--- src/XXindex.tsx
+++ src/XXindex.tsx
@@ -6,12 +6,6 @@
 // const root = createRoot(container!);
 // root.render(<App />);
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
@@ -21,3 +15,9 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
